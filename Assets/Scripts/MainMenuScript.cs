6c8c15224bffae4fd10dda56d11ed908109576f6
GD278 <57197845+GD278@users.noreply.github.com>
2020-03-09 14:35:00 -0500

    Added Button Functionality to MainMenuScript
    Added the functions to control what panels show upon clicking of each button.

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -6,8 +6,40 @@ using UnityEngine.SceneManagement;
 public class MainMenuScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] GameObject MainMenuPanel;
+    [SerializeField] GameObject CreditsPanel;
+    [SerializeField] GameObject ControlsPanel;
+    public void Start()
+    {
+        MainMenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        ControlsPanel.SetActive(false);
+    }
+    public void ShowMainMenu()
+    {
+        MainMenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        ControlsPanel.SetActive(false);
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene("DevLevel");
+    }
+    public void ShowCredits()
+    {
+        MainMenuPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+        ControlsPanel.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void ShowControls()
+    {
+        MainMenuPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+        ControlsPanel.SetActive(true);
     }
 }
