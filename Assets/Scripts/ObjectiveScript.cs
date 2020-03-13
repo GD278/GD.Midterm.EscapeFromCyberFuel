df5455e495d1d6bbc5261ccc23e0c8b3e9e604d5
GD278 <57197845+GD278@users.noreply.github.com>
2020-03-13 10:40:54 -0500

    Created Objective Script
    This script enables a textbox upon level load and disables it on the input of any key.

---
+++ Assets/Scripts/ObjectiveScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectiveScript : MonoBehaviour
+{
+    [SerializeField] Text ObjectiveTextBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObjectiveTextBox.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            ObjectiveTextBox.enabled = false;
+        }
+    }
+}
