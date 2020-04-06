a0e29f996ba4263fbcf0acb54b34c88357467dab
GD278 <57197845+GD278@users.noreply.github.com>
2020-04-06 13:53:27 -0500

    Created Look at Rings Script
    Created and Scripted the arrow to look at the rings so you know where to fly (WIP)

---
+++ Assets/Scripts/LookAtRings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtRings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    ScoreTracker scoreTracker;
+    void Start()
+    {
+        scoreTracker = GetComponent<ScoreTracker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = scoreTracker.rings[scoreTracker.i].transform.position;
+        transform.LookAt(target);
+    }
+}
