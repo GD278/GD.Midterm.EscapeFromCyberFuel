516a004001f65ba7bd3c8061c55fbd917ef28c95
GD278 <57197845+GD278@users.noreply.github.com>
2020-04-06 12:55:24 -0500

    Created Instantiate Rings
    Is called upon via the Score Tracker collision detecter.

---
+++ Assets/Scripts/InstantiateRings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateRings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    ScoreTracker scoreTracker;
+    int i = 0;
+    void Start()
+    {
+        scoreTracker = GetComponent<ScoreTracker>();
+    }
+
+    // Update is called once per frame
+    public void RingInstantiate()
+    {
+        GameObject Ring = Instantiate(scoreTracker.rings[i]);
+    }
+}
