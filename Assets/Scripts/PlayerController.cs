3922dea891bf1ccf8411dbff718605573ca8f807
GD278 <57197845+GD278@users.noreply.github.com>
2020-04-06 09:56:07 -0500

    Maxed out how high the player can fly
    Set the max height the player can fly up to on the y axis to 14. The player will stop moving up.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,38 +22,51 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 forwardMovement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        
+        //Moves player forward
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
         }
+        //Moves player backwards
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.back * Time.deltaTime * speed);
         }
+        //Rotates player left
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.up * Time.deltaTime * -turnSpeed, Space.Self);
            
         }
+        //Rotates player right
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed, Space.Self);
             
         }
-        if (Input.GetKey(KeyCode.Space))
+        //Moves player up
+        if (Input.GetKey(KeyCode.Space) && transform.position.y <= 14.0f)
         {
             playerRB.AddForce(Vector3.up * VertSpeed * Time.deltaTime, ForceMode.Impulse);
         }
+        //Stop player from moving up
         if (Input.GetKeyUp(KeyCode.Space))
         {
             playerRB.velocity = Vector3.zero;
         }
+        //moves player down
         if (Input.GetKey(KeyCode.X))
         {
             playerRB.AddForce(Vector3.down * VertSpeed * Time.deltaTime, ForceMode.Impulse);
         }
+        //stops player from moving down
         if (Input.GetKeyUp(KeyCode.X))
+        {
+            playerRB.velocity = Vector3.zero;
+        }
+
+        //Detects where player is at in sky. If greater than x player will not be able to move up
+        if(transform.position.y >= 14.0f)
         {
             playerRB.velocity = Vector3.zero;
         }
